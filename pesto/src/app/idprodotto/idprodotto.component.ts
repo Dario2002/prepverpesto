8795f629fe19d08bb7c80ed68396385bf7cc1ba3
Dario2002 <banfi.dario@iisgalvanimi.edu.it>
2020-12-09 12:23:36 +0000

    forzadivolontà

---
+++ pesto/src/app/idprodotto/idprodotto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-idprodotto',
+  templateUrl: './idprodotto.component.html',
+  styleUrls: ['./idprodotto.component.css']
+})
+export class IdprodottoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
